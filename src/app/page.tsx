2e2f1ef8f143f9a30daca81ef086af9a8cc165de
sulaiman al farisi <sulaiman@laptop.local>
2025-06-09 11:23:28 +0700

    update features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import ProjectPage from "./projects/project-page";
 import paramService, { ParamService } from "@/server/services/param.service";
 import HostnameCheck from "./settings/server/hostname-check";
+import LimitsPage from "./settings/limits/page";
 
 export default async function Home() {
   return <>
     <ProjectPage />
     <HostnameCheck />
+    <LimitsPage />
   </>;
 }
